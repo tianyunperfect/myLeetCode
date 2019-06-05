fa0e5fcac578d77f20239453c35b5933526e97d2
tianyunperfect <1049368931@qq.com>
2019-06-05 09:08:56 +0800

    第三题

--- src/main/java/com/alvin/leetCode/Code003.java
+++ src/main/java/com/alvin/leetCode/Code003.java
@@ -14,11 +14,23 @@ import java.util.HashSet;
  */
 public class Code003 {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character,Integer> freq = new HashMap<Character, Integer>(256);
-        int l = 0, r = -1;
-        int res = 0;
-
-        return res;
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        boolean[] chars = new boolean[256]; // 256 ASCII code
+        int rst = 0;
+        int start = 0;
+        int end = 0;
+        while (start < s.length()) {
+            while (end < s.length() && !chars[s.charAt(end)]) {
+                chars[s.charAt(end)] = true;
+                rst = Math.max(rst, end - start + 1);
+                end++;
+            }
+            chars[s.charAt(start)] = false;
+            start++;
+        }
+        return rst;
     }
 
     public static void main(String[] args) {
